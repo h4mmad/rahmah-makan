95a26a0e7e15b213429abcb920fd0f21749a02ea
h4mmad <mohammedhammad07@gmail.com>
2023-01-02 18:20:14 +0300

    removed unused vars

--- src/pages/DonatePage.js
+++ src/pages/DonatePage.js
@@ -1,10 +1,8 @@
-import { useState } from "react";
 import Map from "../components/Map";
 
 const DonatePage = () => {
 
-    const [fridgeInfo, setFridgeInfo ] = useState('');
-
+    
     return(
         <div className="container mx-auto px-6 mt-10">
             
