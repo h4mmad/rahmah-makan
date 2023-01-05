5960077a9cb2b7486a71f5f3368dd1d42039102f
h4mmad <mohammedhammad07@gmail.com>
2023-01-06 00:43:39 +0300

    adjusted height of google maps button

--- src/components/GoogleMapsButton.js
+++ src/components/GoogleMapsButton.js
@@ -2,7 +2,7 @@ import MapsIcon from "../images/google-maps.png";
 
 const GoogleMapsButton = ({displayText}) => {
   return (
-    <a href={displayText.googleMapsURL} className="rounded-lg w-full text-center p-2 mt-10 hover:bg-veryLightGray border-2  border-darkGreen font-bold md:hidden">
+    <a href={displayText.googleMapsURL} className="rounded-lg w-full text-center p-2 mt-5 hover:bg-veryLightGray border-2  border-darkGreen font-bold md:hidden">
       
         <img className="inline mr-5 w-10" src={MapsIcon} alt="" />
 
